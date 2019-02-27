b0ee630e2694a6cbf8dc71c8cf6ba4275781f0fb
yangshangzhi <yangshangzhi@yy.com>
2019-02-27 22:17:08 +0800

    modify ocnfig

--- lib/config/index.dart
+++ lib/config/index.dart
@@ -1,6 +1,6 @@
 import 'development.dart' as Development;
 import 'production.dart' as Production;
 
-const bool isPro = true;
+const bool isPro = false;
 
 Object env = isPro ? Production.Config() : Development.Config();
