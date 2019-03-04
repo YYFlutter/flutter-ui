d65ec9d38dfb709cd0fc95d8cb0160d06dff650a
linhaoran <1476589247@qq.com>
2019-03-04 17:26:52 +0800

    feat: BulletBox

---
+++ lib/widget/bulletbox/index.dart
@@ -0,0 +1,14 @@
+import 'package:efox_flutter/store/objects/widget_info.dart';
+
+const nameSpaces = '/bulletbox_';
+
+List widgets = [];
+
+List widgetMap = [
+  ItemListInfo(
+    nameSpaces: nameSpaces,
+    widgetList: widgets,
+    typeName: 'BulletBox',
+    code: 59483 // aspect_ratio
+  )
+];
--- lib/widget/index.dart
+++ lib/widget/index.dart
@@ -1,10 +1,12 @@
 import 'scrollview/index.dart' as scrollview;
 import 'regular/index.dart' as regular;
 import 'navigator/index.dart' as navigator;
+import 'bulletbox/index.dart' as BulletBox;
 List getAllWidgets() {
   List routerMap =[];
   routerMap.addAll(scrollview.widgetMap);
   routerMap.addAll(regular.widgetMap);
   routerMap.addAll(navigator.widgetMap);
+  routerMap.addAll(BulletBox.widgetMap);
   return routerMap;
 }
