af5f5d61194fd0a79df4d1ebf9648f6de0ad1846
linhaoran <1476589247@qq.com>
2019-03-11 15:50:44 +0800

    feat：change Icon

--- lib/widget/form/index.dart
+++ lib/widget/form/index.dart
@@ -1,6 +1,7 @@
 import 'package:efox_flutter/store/objects/widget_info.dart';
 import 'checkbox/index.dart' as CheckBox;
 import 'checkboxlisttile/index.dart' as CheckboxListTile;
+import 'slider/index.dart' as Slider;
 import 'textinput/index.dart' as textinput;
 import 'textfield/index.dart' as textfield;
 import 'switch/index.dart' as switchExample;
@@ -9,7 +10,6 @@ import 'radio/index.dart' as radio;
 import 'formfield/index.dart' as formfield;
 import 'form/index.dart' as form;
 import 'daypicker/index.dart' as daypicker;
-import 'slider/index.dart' as Slider;
 
 const nameSpaces = '/form_';
 
@@ -26,47 +26,47 @@ List widgets = [
   ),
   ItemInfo(
     widget: Slider.Index(),
-    code: 59662, // gavel
+    code: 60229, // golf_course
     title: Slider.Index.title
   ),
   ItemInfo(
     widget: daypicker.Index(),
-    code: 59662, // gavel
+    code: 58345, // gradient
     title: daypicker.Index.title
   ),
   ItemInfo(
     widget: form.Index(),
-    code: 59662, // gavel
+    code: 59526, // group_work
     title: form.Index.title
   ),
   ItemInfo(
     widget: formfield.Index(),
-    code: 59662, // gavel
+    code: 60230, // hot_tub
     title: formfield.Index.title
   ),
   ItemInfo(
     widget: radio.Index(),
-    code: 59662, // gavel
+    code: 58693, // local_florist 
     title: radio.Index.title
   ),
   ItemInfo(
     widget: rawkeyboard.Index(),
-    code: 59662, // gavel
+    code: 58698, // local_laundry_service
     title: rawkeyboard.Index.title
   ),
   ItemInfo(
     widget: switchExample.Index(),
-    code: 59662, // gavel
+    code: 58706, // local_pizza
     title: switchExample.Index.title
   ),
   ItemInfo(
     widget: textfield.Index(),
-    code: 59662, // gavel
+    code: 58715, // map
     title: textfield.Index.title
   ),
   ItemInfo(
     widget: textinput.Index(),
-    code: 59662, // gavel
+    code: 58146, // memory
     title: textinput.Index.title
   )
 ];
