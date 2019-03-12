5fa4af62656eed31af419cf4d5cd6f6bd284475f
wanwusangzhi <609780590@qq.com>
2019-03-12 22:10:12 +0800

    feat:add draw component

--- lib/widget/navigator/drawer/demo.dart
+++ lib/widget/navigator/drawer/demo.dart
@@ -18,7 +18,70 @@ class _IndexState extends State<Index> {
         title: Text('Drawer'),
       ),
       body: Center(
-        child: Text('更新中'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Builder(
+              builder: (context) {
+                return RaisedButton(
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  child: Text("点击滑出左侧菜单"),
+                );
+              },
+            ),
+            Text('从左侧中滑出菜单组件'),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 38.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ClipOval(
+                      child: Image.asset(
+                        "assets/imgs/avatar.png",
+                        width: 80,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Guest",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text('Change Account'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Setting'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.close),
+                    title: Text('点击关闭菜单'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
