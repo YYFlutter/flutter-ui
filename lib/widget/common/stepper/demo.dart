c0653334b024b2d3483cee2ccda2187bcfca9b55
sam <sam@haorandeMacBook-Pro.local>
2019-03-31 19:42:38 +0800

    feat: stepper

--- lib/widget/common/stepper/demo.dart
+++ lib/widget/common/stepper/demo.dart
@@ -6,6 +6,7 @@ class Index extends StatefulWidget {
 }
 
 class _IndexState extends State<Index> {
+  int _currentStep = 0;
   @override
   void initState() {
     super.initState();
@@ -17,9 +18,57 @@ class _IndexState extends State<Index> {
       appBar: AppBar(
         title: Text('Stepper'),
       ),
-      body: Center(
-        child: Text('更新中'),
-      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Theme(
+              data: Theme.of(context).copyWith(
+                primaryColor: Colors.black
+              ),
+              child: Stepper(
+                currentStep: _currentStep,
+                onStepTapped: (int value) {
+                  setState(() {
+                    _currentStep = value;
+                  });
+                },
+                onStepContinue: () {
+                  setState(() {
+                    _currentStep < 2 ? _currentStep += 1 : _currentStep = 0;
+                  });
+                },
+                onStepCancel: (){
+                  setState(() {
+                    _currentStep > 0 ? _currentStep -= 1 : _currentStep = 0;
+                  });
+                },
+                steps: [
+                  Step(
+                    title: Text('Login'),
+                    subtitle: Text('Login first'),
+                    content: Text('Confirm your information'),
+                    isActive: _currentStep == 0
+                  ),
+                  Step(
+                    title: Text('Choose plan'),
+                    subtitle: Text('Choose your plan'),
+                    content: Text('Confirm your information'),
+                    isActive: _currentStep == 1
+                  ),
+                  Step(
+                    title: Text('Confirm payment'),
+                    subtitle: Text('Confirm yoir payment method'),
+                    content: Text('Confirm your information'),
+                    isActive: _currentStep == 2
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
