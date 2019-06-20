0b7adebc91ee2329bda149b417523801400ec30c
KenZR <ckken@qq.com>
2019-06-20 17:39:15 +0800

    fix:修复iphonex底部问题，修复登陆唔返回问题

--- lib/page/app_login/index.dart
+++ lib/page/app_login/index.dart
@@ -66,134 +66,135 @@ class _IndexState extends State<Index> {
 
   @override
   Widget build(BuildContext ctx) {
-    return WillPopScope(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            AppLocalizations.$t('common.login'),
-            textAlign: TextAlign.center,
-          ),
-          automaticallyImplyLeading: false,
+    // return WillPopScope(
+    //   child: ,
+    //   onWillPop: () {
+    //     beforeDispose();
+    //   },
+    // );
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          AppLocalizations.$t('common.login'),
+          textAlign: TextAlign.center,
         ),
-        body: Builder(builder: (BuildContext context) {
-          return SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 50, horizontal: 24),
-              child: Form(
-                key: _formKey,
-                autovalidate: true,
-                child: Column(
-                  children: <Widget>[
-                    renderGithubImage(),
-                    TextFormField(
-                      controller: nameCtl,
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        labelText: AppLocalizations.$t('login.account'),
-                        hintText: AppLocalizations.$t('login.account_tips'),
-                        icon: Icon(Icons.person),
-                      ),
-                      validator: (v) {
-                        return v.trim().length > 0
-                            ? null
-                            : AppLocalizations.$t('login.account_error_tips');
-                      },
+        // automaticallyImplyLeading: false,
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 50, horizontal: 24),
+            child: Form(
+              key: _formKey,
+              autovalidate: true,
+              child: Column(
+                children: <Widget>[
+                  renderGithubImage(),
+                  TextFormField(
+                    controller: nameCtl,
+                    autofocus: false,
+                    decoration: InputDecoration(
+                      labelText: AppLocalizations.$t('login.account'),
+                      hintText: AppLocalizations.$t('login.account_tips'),
+                      icon: Icon(Icons.person),
                     ),
-                    TextFormField(
-                      controller: pwdCtl,
-                      decoration: InputDecoration(
-                        labelText: AppLocalizations.$t('login.password'),
-                        hintText: AppLocalizations.$t('login.password_tips'),
-                        icon: Icon(Icons.lock),
-                      ),
-                      obscureText: true,
-                      validator: (v) {
-                        return v.trim().length > 0
-                            ? null
-                            : AppLocalizations.$t('login.password_error_tips');
-                      },
+                    validator: (v) {
+                      return v.trim().length > 0
+                          ? null
+                          : AppLocalizations.$t('login.account_error_tips');
+                    },
+                  ),
+                  TextFormField(
+                    controller: pwdCtl,
+                    decoration: InputDecoration(
+                      labelText: AppLocalizations.$t('login.password'),
+                      hintText: AppLocalizations.$t('login.password_tips'),
+                      icon: Icon(Icons.lock),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 50),
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: RaisedButton(
-                              padding: EdgeInsets.all(15),
-                              color: Theme.of(context).primaryColor,
-                              textColor: Theme.of(context)
-                                  .primaryTextTheme
-                                  .title
-                                  .color,
-                              child: Text(
-                                AppLocalizations.$t('common.login'),
-                              ),
-                              onPressed: () async {
-                                if ((_formKey.currentState as FormState)
-                                    .validate()) {
-                                  await Store.value<UserModel>(context)
-                                      .loginController(context, {
-                                    'name': nameCtl.text.trim(),
-                                    'pwd': pwdCtl.text.trim()
-                                  });
-                                  await Store.value<UserModel>(context).getUserStar();
-                                }
-                              },
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    obscureText: true,
+                    validator: (v) {
+                      return v.trim().length > 0
+                          ? null
+                          : AppLocalizations.$t('login.password_error_tips');
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Row(
                       children: <Widget>[
-                        GestureDetector(
-                          child: Text(
-                            'Github账户登录说明',
-                            style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              textBaseline: TextBaseline.ideographic,
-                              decorationColor: Color(0xff000000),
+                        Expanded(
+                          child: RaisedButton(
+                            padding: EdgeInsets.all(15),
+                            color: Theme.of(context).primaryColor,
+                            textColor:
+                                Theme.of(context).primaryTextTheme.title.color,
+                            child: Text(
+                              AppLocalizations.$t('common.login'),
                             ),
+                            onPressed: () async {
+                              if ((_formKey.currentState as FormState)
+                                  .validate()) {
+                                await Store.value<UserModel>(context)
+                                    .loginController(context, {
+                                  'name': nameCtl.text.trim(),
+                                  'pwd': pwdCtl.text.trim()
+                                });
+                                await Store.value<UserModel>(context)
+                                    .getUserStar();
+                              }
+                            },
                           ),
-                          onTap: () {
-                            renderOverlay(" Text 1");
-                          },
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        GestureDetector(
-                          child: Text(
-                            '软件许可及服务协议',
-                            style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              textBaseline: TextBaseline.ideographic,
-                              decorationColor: const Color(0xff000000),
-                            ),
-                          ),
-                          onTap: () {
-                            renderOverlay(" Text 2");
-                          },
-                        ),
+                        )
                       ],
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      GestureDetector(
+                        child: Text(
+                          'Github账户登录说明',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            textBaseline: TextBaseline.ideographic,
+                            decorationColor: Color(0xff000000),
+                          ),
+                        ),
+                        onTap: () {
+                          renderOverlay(" Text 1");
+                        },
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        child: Text(
+                          '软件许可及服务协议',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            textBaseline: TextBaseline.ideographic,
+                            decorationColor: const Color(0xff000000),
+                          ),
+                        ),
+                        onTap: () {
+                          renderOverlay(" Text 2");
+                        },
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
-          );
-        }),
-      ),
-      onWillPop: () {
-        beforeDispose();
-      },
+          ),
+        );
+      }),
     );
   }
+
   beforeDispose() {
     if (_overlayEntry != null) {
       _overlayEntry.remove();
